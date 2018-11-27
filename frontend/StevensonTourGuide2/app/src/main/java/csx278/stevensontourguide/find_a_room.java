3450b467f09abbb8b866346881404d5b7be1682f
AkanEyoh <akaninyene.e.eyoh@vanderbilt.edu>
2018-11-27 10:35:35 -0600

    frontend_start_app_interface

---
+++ frontend/StevensonTourGuide2/app/src/main/java/csx278/stevensontourguide/find_a_room.java
@@ -0,0 +1,13 @@
+package csx278.stevensontourguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class find_a_room extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_a_room);
+    }
+}
